99735dd6394639bcd31ff6ba57d6e8c124760b6f
ArtemKruykov <artem_kruykov@mail.ru>
2025-10-17 10:09:36 +0300

    tfctfv

---
+++ Task2Test/Task2Test.cpp
@@ -0,0 +1,15 @@
+﻿#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Task2Test
+{
+	TEST_CLASS(Task2Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
